8885b6b6d710acf3604dc66df6e45dd8b17cbf45
jayantt23 <jayant.sharma@iiitb.ac.in>
2026-04-14 20:29:34 +0530

    add basic folder structure

---
+++ dashboard/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title='poli-stance', layout='wide')
+st.title('poli-stance: Explainable Political Analysis')
+st.info('Waiting for model output JSON...')
